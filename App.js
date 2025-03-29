1e48c5239e3ce60a99fd5a952cddd3762b4ed0aa
Anushadevarapalli <anusha522.d@gmail.com>
2025-03-29 14:55:13 +0530

    Add files via upload
    fist files committed

---
+++ App.js
@@ -0,0 +1,10 @@
+// Define a component called Greetings
+function Greetings() {
+    return <h1>Hello readers, Thankyou for reading this blog !</h1>;
+  }
+
+  // Render the component to the DOM
+  ReactDOM.render(
+    <Greetings />,
+    document.getElementById("root")
+  );
